09119a04e514d2e56c509b1db87383e466aa174f
João Vitor <joaovitordasilvafreitas@gmail.com>
2025-02-24 11:48:10 -0300

    Inicio das configurações em JS

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const inputTask = document.querySelector('.input_task');
 const btn_add = document.querySelector('.btn_add');
+const btn_finishTask = document.querySelector('.btn_finishTask');
+const btn_dropTask = document.querySelector('.btn_dropTask');
+
+
+
+function add_task(){
+    receiver()
+}
+
+function receiver(){
+    //alert("o texto foi capturado")
+    var task = inputTask.value;
+    inputTask.value = "";
+    alert(`o valor recuperado foi: ${task}`)
+}
